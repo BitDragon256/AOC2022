99a1397737d612e04306d0053a8d845ded8635ff
Benedict SONDERSHAUS <sonderbe@r109-ws09.qg-moessingen.de>
2022-12-01 08:40:01 +0100

    Day 1 done

--- Day1/day1.cpp
+++ Day1/day1.cpp
@@ -10,28 +10,33 @@ int main()
 {
     cout << "start" << endl;
 
-    ifstream file("./input.txt");
+    ifstream file("input.txt");
 
     if (file.is_open())
     {
         vector<int> calories;
-        bool newElf = false;
+        bool newElf = true;
         string line;
         while (getline(file, line))
         {
-            if (line == "")
+            if (line.size() == 0)
             {
                 newElf = true;
                 continue;
             }
-
+            
             if (newElf)
+            {
                 calories.push_back(stoi(line));
+                newElf = false;
+            }
             else
             {
-                calories[calories.size()] += stoi(line);
+                calories[calories.size() - 1] += stoi(line, nullptr, 10);
             }
         }
+
+        // Part 1
         int max = 0;
         for (int c : calories)
         {
@@ -40,6 +45,34 @@ int main()
         }
 
         cout << max << endl;
+
+        // Part 2
+        int top3[3] = { 0,0,0 };
+        for (int c : calories)
+        {
+            cout << c;
+            for (int i = 0; i < 3; i++)
+            {
+                cout << " | " << top3[i];
+                if (c > top3[i])
+                {
+                    for (int j = 2; j >= i + 1; j--)
+                    {
+                        top3[j] = top3[j - 1];
+                    }
+                    top3[i] = c;
+                    break;
+                }
+            }
+            cout << endl;
+        }
+
+        int topSum = 0;
+        for (int v : top3)
+        {
+            topSum += v;
+        }
+        cout << topSum << endl;
     }
     else
     {
