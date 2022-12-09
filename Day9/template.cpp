53a1b8b9ab0ae22616a6a3864164c8dd64156723
Benedict <bergben44@gmail.com>
2022-12-09 06:41:22 +0100

    WIP Day9

--- Day9/template.cpp
+++ Day9/template.cpp
@@ -2,10 +2,12 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <map>
 
 #include "../Util.h"
 
 using namespace std;
+using namespace Util;
 
 class Solver
 {
@@ -39,9 +41,27 @@ int main()
     }
 }
 
+map<char, vec2> directions = { 
+    { 'R', vec2(1, 0) },
+    { 'L', vec2(-1, 0) },
+    { 'U', vec2(0, 1) },
+    { 'D', vec2(0, -1) }
+ };
+
 void Solver::part01(vector<string> input)
 {
-    
+    vec2 head, tail;
+
+    char move;
+    uint len;
+    for (string s : input)
+    {
+        move = s.at(0);
+        len = stoi(split(s, " ")[1]);
+
+        head += directions[move];
+
+    }
 }
 
 void Solver::part02(vector<string> input)
