b99dce5560e623a2487dcee8b62dc59998b43e81
lg20 <bergben44@gmail.com>
2022-12-14 13:31:39 +0100

    WIP Day14 Beautifying

--- Day14/template.cpp
+++ Day14/template.cpp
@@ -15,9 +15,8 @@ public:
 };
 
 bool* grid;
-const vec2 gridSize = vec2(400, 166 + 1 + 2);
-const int delta = gridSize.x / 2 - 61;
-//const vec2 gridSize = vec2(10, 10);
+vec2 gridSize;
+vec2 sandStart;
 
 void printGrid(vec2);
 
@@ -47,12 +46,38 @@ int main()
         for (string coord : rockLine)
         {
             split = Util::split(coord, ",");
-            coords[coords.size() - 1].push_back( vec2(stoi(split[0]) - 460 + delta, stoi(split[1])) ); // X: 460 - 521 | Y: 166
+            coords[coords.size() - 1].push_back( vec2(stoi(split[0]), stoi(split[1])) ); // X: 460 - 521 | Y: 166
         }
     }
 
-    grid = new bool[gridSize.x * gridSize.y];
-    for (int i = 0; i < gridSize.x * gridSize.y; i++)
+    vec2 min = vec2(10000, 10000); vec2 max = vec2(-1, -1);
+    for (vector<vec2> rockLine : coords)
+    {
+        for (vec2 v : rockLine)
+        {
+            if (v.x > max.x)
+                max.x = v.x;
+            if (v.y > max.y)
+                max.y = v.y;
+            if (v.x < min.x)
+                min.x = v.x;
+            if (v.y < min.y)
+                min.y = v.y;
+        }
+    }
+    vec2 span = max - min;
+    gridSize = vec2(max.y * 2 + 1, max.y + 1 + 2);
+    uint delta = (gridSize.x - span.x) / 2;
+    sandStart = vec2(500 - min.x + delta, 0);
+
+    //cout << "Span: " << span.x << " " << span.y << endl;
+    cout << "Span: " << span << endl;
+    cout << "Gridsize: " << gridSize.x << " " << gridSize.y << " " << gridSize.sqrMagnitude() << endl;
+    cout << "Delta: " << delta << endl;
+    cout << "Sandstart: " << sandStart.x << " " << sandStart.y << endl;
+
+    grid = new bool[gridSize.sqrMagnitude()];
+    for (int i = 0; i < gridSize.sqrMagnitude(); i++)
     {
         grid[i] = false;
     }
@@ -64,7 +89,7 @@ int main()
         {
             maxPos = vec2::max(rock[i - 1], rock[i]);
             minPos = vec2::min(rock[i - 1], rock[i]);
-            for (int x = minPos.x; x <= maxPos.x; x++)
+            for (int x = minPos.x + delta - min.x; x <= maxPos.x + delta - min.x; x++)
             {
                 for (int y = minPos.y; y <= maxPos.y; y++)
                 {
@@ -73,47 +98,19 @@ int main()
             }
         }
     }
-    for (int x = 0; x < gridSize.x; x++)
-    {
-        int y = gridSize.y - 1;
-        grid[x + y * gridSize.x] = true;
-    }
-
-    printGrid(vec2(0, 0));
-
-    // delete[] grid;
-    // return 0;
-
-    // uint minX = 10000; uint maxX = 0; uint minY = 10000; uint maxY = 0;
-    // for (vec2 v : coords)
-    // {
-    //     cout << v.x << " " << v.y << endl;
-    //     if (v.x > maxX)
-    //         maxX = v.x;
-    //     if (v.y > maxY)
-    //         maxY = v.y;
-    //     if (v.x < minX)
-    //         minX = v.x;
-    //     if (v.y < minY)
-    //         minY = v.y;
-    // }
-
-    // cout << minX << " " << maxX << endl << minY << " " << maxY << endl;
 
     Solver s;
     s.part01();
-    //s.part02();
+    s.part02();
 
     delete[] grid;
 }
 
 void printGrid(vec2 sand)
 {
-    string s;
-    cin >> s;
     cout << endl << endl;
 
-    for (int i = 0; i < gridSize.x * gridSize.y; i++)
+    for (int i = 0; i < gridSize.sqrMagnitude(); i++)
     {
         if (i % gridSize.x == 0)
             cout << endl;
@@ -130,15 +127,12 @@ void printGrid(vec2 sand)
 
 void Solver::part01()
 {
-    vec2 sandStart = vec2(500 - 460 + delta, 0);
-    //vec2 sandStart = vec2(500 - 494, 0);
     vec2 sand = sandStart;
     uint sandCount = 0;
     uint8_t state = 0;
 
     while (true)
     {
-        //printGrid(sand);
         switch (state)
         {
             case 0:
@@ -154,8 +148,6 @@ void Solver::part01()
                 sand.y--;
                 sand.x--;
 
-                //printGrid(sand);
-
                 grid[sand.x + sand.y * gridSize.x] = true;
                 sand = sandStart;
                 sandCount++;
@@ -163,10 +155,8 @@ void Solver::part01()
                 break;
         }
         
-        // if (sand.y >= gridSize.y)
-        if (grid[sandStart.x + sandStart.y * gridSize.x])
+        if (sand.y >= gridSize.y - 2) // ignore floor of part 2
         {
-            printGrid(vec2(0, 0));
             break;
         }
         if (grid[sand.x + sand.y * gridSize.x])
@@ -181,5 +171,53 @@ void Solver::part01()
 
 void Solver::part02()
 {
-    
+    // add the floor
+    for (int x = 0; x < gridSize.x; x++)
+    {
+        int y = gridSize.y - 1;
+        grid[x + y * gridSize.x] = true;
+    }
+
+    vec2 sand = sandStart;
+    uint sandCount = 0;
+    uint8_t state = 0;
+
+    while (true)
+    {
+        switch (state)
+        {
+            case 0:
+                sand.y += 1;
+                break;
+            case 1:
+                sand.x--;
+                break;
+            case 2:
+                sand.x += 2;
+                break;
+            case 3:
+                sand.y--;
+                sand.x--;
+                
+                grid[sand.x + sand.y * gridSize.x] = true;
+                sand = sandStart;
+                sandCount++;
+                state = 0;
+                break;
+        }
+
+        
+        if (grid[sandStart.x + sandStart.y * gridSize.x])
+        {
+            break;
+        }
+        if (grid[sand.x + sand.y * gridSize.x])
+        {
+            //cout << sand.x << " " << sand.y << " " << (sand.x + sand.y * gridSize.x) << endl;
+            state++;
+            continue;
+        }
+        state = 0;
+    }
+    cout << sandCount << endl;
 }
