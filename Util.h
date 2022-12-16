09f86722d1a34da1153ed8dbf719d9a0019f37cb
lg20 <bergben44@gmail.com>
2022-12-16 20:01:03 +0100

    Util multisplit

--- Util.h
+++ Util.h
@@ -25,6 +25,30 @@ namespace Util
         return res;
     }
 
+    vector<string> splitMulti(string s, vector<string> dels)
+    {
+        vector<string> res;
+        res.push_back(s);
+        size_t pos;
+        size_t size;
+        for (string del : dels)
+        {
+            size = res.size();
+            for (int i = 0; i < size; i++)
+            {
+                while ((pos = res[i].find(del)) != string::npos)
+                {
+                    res.push_back(res[i].substr(0, pos));
+                    res[i].erase(0, pos + del.length());
+                }
+                if (res[i] != "")
+                    res.push_back(res[i]);
+            }
+            res.erase(res.begin(), res.begin() + size);
+        }
+        return res;
+    }
+
     void printStacks(vector<stack<char>*> stacks)
     {
         for (auto stack : stacks)
